5c5b67c4052368b88ebf70ea7ceb677604487433
Carmina <cvdsoto@gmail.com>
2020-04-27 10:23:51 +1000

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
-import React from "react"
+import React from "react";
+import Header from '../components/Header';
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div>
+    <h1>Recipe Finder!</h1>
+    <Header headerText="Welcome" />
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut justo vitae nulla eleifend consequat et at ante. Integer nec efficitur erat. Aliquam nec tempus dolor. Curabitur id turpis id ligula efficitur scelerisque. Maecenas vel ex ac purus placerat cursus.</p>
+    <img src="https://truffle-assets.imgix.net/96f876c0-fallback-food-video-asset.jpg?auto=compress,format&fm=pjpg&w=1200" alt="food at table"/>
+  </div>
+)
