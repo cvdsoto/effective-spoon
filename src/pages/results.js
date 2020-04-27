0b7ca13392cb3fcfe01429c1689768d29281b4ee
Carmina <cvdsoto@gmail.com>
2020-04-27 18:11:40 +1000

    set-up API and hide API Keys

---
+++ src/pages/results.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+export default class Results extends Component {
+  constructor(props){
+    super();
+    this.state = {
+      ingredients: props.location.state.ingredients,
+      recipes: null
+    }
+  }
+
+  componentDidMount(){
+    const URL = `https://api.edamam.com/search?q=${this.state.ingredients}&app_id=${process.env.GATSBY_APP_ID}&app_key=${process.env.GATSBY_APP_KEY}&from=0&to=10`;
+    axios.get(URL).then(results => {
+      this.setState({recipes: results.data.hits});
+    })
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>{this.state.ingredients}</h1>
+        <Recipe recipes= {this.state.recipes} />
+      </div>
+    )
+  }
+}
+
+const Recipe = (props) => {
+  return(
+    <div>
+    {console.log(props.recipes)}
+    </div>
+  )
+}
--- src/pages/search.js
+++ src/pages/search.js
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from '../components/Header';
+import {navigate} from 'gatsby';
 
-export default () => (
+export default () => {
+  const [ingredients, setIngredients] = useState('');
+
+  const _handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/results`, {state: {ingredients}});
+  }
+
+  return (
   <div>
     <Header headerText="Search for a Recipe" />
-    
+    <form onSubmit={_handleSubmit}>
+      <label>
+        Ingredients:
+        <input type="search"
+                value={ingredients}
+                onChange={event => setIngredients(event.target.value)}/>
+      </label>
+      <label>
+        <input type="checkbox" /> Balanced
+      </label>
+      <label>
+        <input type="checkbox" /> High Protein
+      </label>
+      <label>
+        <input type="checkbox" /> Low-fat
+      </label>
+      <label>
+        <input type="checkbox" /> Low-Carb
+      </label>
+      <label>
+        <input type="checkbox" /> Vegan
+      </label>
+      <label>
+        <input type="checkbox" /> Vegetarian
+      </label>
+      <label>
+        <input type="checkbox" /> Sugar-Conscious
+      </label>
+      <label>
+        <input type="checkbox" /> Peanut-free
+      </label>
+      <label>
+        <input type="checkbox" /> Tree-nut-free
+      </label>
+      <label>
+        <input type="checkbox" /> Alcohol-free
+      </label>
+
+      <input type="submit" value="Search" />
+    </form>
   </div>
-)
+  )
+}
